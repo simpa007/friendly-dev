bb45a480f6e86724cef09383b9d6717bd3a0b359
Simpa <simpaimmey@gmail.com>
2026-03-11 04:09:06 -0700

    uploaded my project from json-server

--- app/routes/projects/index.tsx
+++ app/routes/projects/index.tsx
@@ -1,5 +1,27 @@
-import React from "react";
+import type { Route } from "./+types/index";
+import type { Project } from "~/types";
+import ProjectCard from "~/component/ProjectCard";
 
-export default function ProjectPage() {
-	return <>ProjectPage</>;
+export async function loader({
+	request,
+}: Route.LoaderArgs): Promise<{ projects: Project[] }> {
+	const res = await fetch("http://localhost:8000/projects");
+	const data = await res.json();
+
+	return { projects: data };
+}
+
+export default function ProjectPage({ loaderData }: Route.ComponentProps) {
+	const { projects } = loaderData as { projects: Project[] };
+
+	return (
+		<>
+			<h1 className="text-3xl text-white font-bold mb-8">Projects</h1>
+			<div className="grid gap-6 sm:grid-cols-2">
+				{projects.map((project) => (
+					<ProjectCard key={project.id} project={project} />
+				))}
+			</div>
+		</>
+	);
 }
