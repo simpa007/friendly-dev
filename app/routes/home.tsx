7101462faee976116a83c15d415b258f51e81daf
Simpa <simpaimmey@gmail.com>
2026-04-11 07:07:38 -0700

    added featured project to the home page

--- app/routes/home.tsx
+++ app/routes/home.tsx
@@ -1,4 +1,6 @@
 import type { Route } from "./+types/home";
+import type { Project } from "~/types";
+import FeaturedProjects from "~/component/FeaturedProjects";
 
 export function meta({}: Route.MetaArgs) {
 	return [
@@ -7,6 +9,21 @@ export function meta({}: Route.MetaArgs) {
 	];
 }
 
-export default function Home() {
-	return <div>Homepage</div>;
+export async function loader({
+	request,
+}: Route.LoaderArgs): Promise<{ projects: Project[] }> {
+	const res = await fetch(`${import.meta.env.VITE_API_URL}/projects`);
+	const data = await res.json();
+
+	return { projects: data };
+}
+
+export default function Home({ loaderData }: Route.ComponentProps) {
+	const { projects } = loaderData;
+
+	return (
+		<>
+			<FeaturedProjects projects={projects} count={2} />
+		</>
+	);
 }
