ce42bf05f7c5362cb09779fd3edf4f914f446c55
Simpa <simpaimmey@gmail.com>
2026-04-17 07:16:37 -0700

    added blog page as weel as the latest blog on the home page

--- app/routes/blog/index.tsx
+++ app/routes/blog/index.tsx
@@ -1,7 +1,9 @@
-import React from "react";
+import React, { useState } from "react";
 import type { Route } from "./+types/index";
 import type { PostMeta } from "~/types";
 import PostCard from "~/component/PostCard";
+import Pagination from "~/component/Pagination";
+import PostFilter from "~/component/PostFilter";
 
 export async function loader({
 	request,
@@ -11,19 +13,62 @@ export async function loader({
 	if (!res.ok) throw new Error("Failed to fetch data");
 
 	const data = await res.json();
+	data.sort((a: PostMeta, b: PostMeta) => {
+		return new Date(b.date).getTime() - new Date(a.date).getTime();
+	});
 
 	return { posts: data };
 }
 
 export default function Blog({ loaderData }: Route.ComponentProps) {
+	const [searchQuery, setSearchQuery] = useState("");
+	const [currentPage, setCurrentPage] = useState(1);
+	const projectsPerPage = 10;
 	const { posts } = loaderData;
+
+	//Calculate total pages
+	const totalPages = Math.ceil(posts.length / projectsPerPage);
+
+	//Get Current pages projects
+	const indexOfLast = currentPage * projectsPerPage;
+	const indexOfFirst = indexOfLast - projectsPerPage;
+	const currentPosts = posts.slice(indexOfFirst, indexOfLast);
+
+	//filter posts by Search
+
+	const filterPosts = currentPosts.filter((post) => {
+		const query = searchQuery.toLowerCase();
+		return (
+			post.title.toLowerCase().includes(query) ||
+			post.excerpt.toLowerCase().includes(query)
+		);
+	});
 
 	return (
 		<div className="max-w-3xl mx-auto mt-10 px-6 py-6 bg-gray-900">
 			<h2 className="text-3xl text-white font-bold mb-8">Blog</h2>
-			{posts.map((post) => (
-				<PostCard key={post.slug} post={post} />
-			))}
+
+			<PostFilter
+				searchQuery={searchQuery}
+				onSearchChange={(value) => {
+					setSearchQuery(value);
+					setCurrentPage(1);
+				}}
+			/>
+
+			<div className="space-y-8">
+				{filterPosts.length === 0 ? (
+					<p className="text-gray-400 text-center">No posts found</p>
+				) : (
+					filterPosts.map((post) => <PostCard key={post.slug} post={post} />)
+				)}
+			</div>
+
+			<Pagination
+				totalPages={totalPages}
+				currentPage={currentPage}
+				onPageChange={setCurrentPage}
+			/>
 		</div>
 	);
 }
--- app/routes/home.tsx
+++ app/routes/home.tsx
@@ -1,7 +1,9 @@
 import type { Route } from "./+types/home";
 import type { Project } from "~/types";
+import type { PostMeta } from "~/types";
 import FeaturedProjects from "~/component/FeaturedProjects";
 import AboutPreview from "~/component/AboutPreview";
+import LatestPosts from "~/component/LatestPosts";
 
 export function meta({}: Route.MetaArgs) {
 	return [
@@ -12,20 +14,33 @@ export function meta({}: Route.MetaArgs) {
 
 export async function loader({
 	request,
-}: Route.LoaderArgs): Promise<{ projects: Project[] }> {
-	const res = await fetch(`${import.meta.env.VITE_API_URL}/projects`);
-	const data = await res.json();
+}: Route.LoaderArgs): Promise<{ projects: Project[]; posts: PostMeta[] }> {
+	const url = new URL(request.url);
+	const [projectRes, postRes] = await Promise.all([
+		fetch(`${import.meta.env.VITE_API_URL}/projects`),
+		fetch(new URL("/posts-meta.json", url)),
+	]);
 
-	return { projects: data };
+	if (!projectRes.ok || !postRes.ok)
+		throw new Error("Failed to fetch projects or posts");
+
+	const [projects, posts] = await Promise.all([
+		projectRes.json(),
+		postRes.json(),
+	]);
+
+	return { projects, posts };
 }
 
 export default function Home({ loaderData }: Route.ComponentProps) {
-	const { projects } = loaderData;
+	const { projects, posts } = loaderData;
 
 	return (
 		<>
 			<FeaturedProjects projects={projects} count={2} />
+
 			<AboutPreview />
+			<LatestPosts posts={posts} count={3} />
 		</>
 	);
 }
